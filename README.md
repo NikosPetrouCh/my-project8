b223b0f548075c5b3752847b08f5085e808000d2
Nikos <npetrouch@gmail.com>
2025-05-21 04:59:07 +0300

    φεfeat: add gitignorem readme and hello world class

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my-project8
+# **Requirements**
+
+---
+- JDK
+
+# **Usage**
+
+1. Just run the HelloWorld class of the app.
---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch8;
+
+/**
+ * This project created to show how
+ * to make a git project. And then how
+ * to stage and commit changes in local repo.
+ * And then how to push to Github
+ */
+public class HelloWorld {
+}
